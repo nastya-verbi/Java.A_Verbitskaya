798ca4264cf29c428a2e9edc561c640f46204f58
nastya_verbi <blissena49@gmail.com>
2021-08-01 10:46:14 +0400

    Исправление ДЗ №6

--- src/Lesson06/homework/AnimalsAPP.java
+++ src/Lesson06/homework/AnimalsAPP.java
@@ -6,12 +6,10 @@ public class AnimalsAPP {
 
     public static void main(String[] args) {
 
-        Dog dog = new Dog("Бобик");
-        Dog dog1 = new Dog("Шарик");
-        Cat cat = new Cat("Снежок");
-        Cat cat1 = new Cat("Пушок");
-        Cat cat2 = new Cat("Барсик");
-
+        Dog dog = new Dog("Бобик", 500, 10);
+        Dog dog1 = new Dog("Шарик", 500, 10);
+        Cat cat = new Cat("Снежок", 200, 0);
+        Cat cat1 = new Cat("Пушок", 200, 0);
 
         dog.doRun(100);
         dog.doSwim(5);
@@ -19,10 +17,8 @@ public class AnimalsAPP {
         dog1.doSwim(15);
         cat.doRun(500);
         cat.doSwim(10);
-        cat1.doRun(200);
-        cat1.doSwim(5);
-        cat2.doRun(0);
-        cat2.doSwim(0);
+        cat1.doRun(0);
+        cat1.doSwim(0);
 
         System.out.println("Количество всех животных: " + Animals.getCountAnimals());
         System.out.println("Количество собак: " + Cat.getCountCats());
