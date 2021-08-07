08a5ddd834b5ff21b6b37aa6586a64fa2a5e32e5
nastya_verbi <blissena49@gmail.com>
2021-08-07 18:24:57 +0400

    Обновление

--- src/Lesson06/homework/Cat.java
+++ src/Lesson06/homework/Cat.java
@@ -3,12 +3,10 @@ package Lesson06.homework;
 import Lesson06.homework.model.Animals;
 
 public class Cat extends Animals {
-    private int maxRun = 200;
     private static int count = 0;
 
-
-    public Cat(String name) {
-        super(name);
+    public Cat(String name, int maxRun, int maxSwim) {
+        super(name, maxRun, maxSwim);
         count++;
     }
 
@@ -17,15 +15,8 @@ public class Cat extends Animals {
     }
 
     @Override
-    public void doRun(int pathLength) {
-        if (pathLength > maxRun)
-            System.out.println(name + " пробежал только 200м. Остальные " + (pathLength - maxRun) + "м не осилил");
-        else super.doRun(pathLength);
-    }
-
-    @Override
     public void doSwim(int pathLength) {
-        if (pathLength > 0) System.out.println(name + " утонул, потому что не умел плавать");
+        if (pathLength > maxSwim) System.out.println(name + " утонул, потому что не умел плавать");
         else super.doSwim(pathLength);
     }
 }
--- src/Lesson06/homework/Dog.java
+++ src/Lesson06/homework/Dog.java
@@ -3,12 +3,10 @@ package Lesson06.homework;
 import Lesson06.homework.model.Animals;
 
 public class Dog extends Animals {
-    private int maxRun = 500;
-    private int maxSwim = 10;
     private static int count = 0;
 
-    public Dog(String name) {
-        super(name);
+    public Dog(String name, int maxRun, int maxSwim) {
+        super(name, maxRun, maxSwim);
         count++;
     }
 
@@ -16,17 +14,5 @@ public class Dog extends Animals {
         return count;
     }
 
-    @Override
-    public void doRun(int pathLength) {
-           if (pathLength > maxRun)
-               System.out.println(name + " пробежал только 500м. Остальные " + (pathLength - maxRun) + "м не осилил");
-           else super.doRun(pathLength);
-    }
 
-    @Override
-    public void doSwim(int pathLength) {
-        if (pathLength > maxSwim)
-            System.out.println(name + " проплыл только 10м. Остальные " + (pathLength - maxSwim) + "м не осилил");
-        else super.doSwim(pathLength);
-    }
 }
